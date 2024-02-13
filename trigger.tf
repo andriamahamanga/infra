5436a46d36face05ad3d51bfa3096af8d8cb4651
rija <rija@wylog.com>
2024-02-13 11:01:56 +0300

    debug info

--- trigger.tf
+++ trigger.tf
@@ -38,7 +38,7 @@ resource "google_cloudbuild_trigger" "build_trigger_terraform_feature" {
       args       = ["-c", "terraform plan var-file=config-${var.env}/terraform.tfvars"]
     }
   }
-
+  service_account = google_service_account.our_sa_terraform_build
 }
 resource "google_cloudbuild_trigger" "build_trigger_terraform" {
   location = "us-central1"
@@ -65,5 +65,5 @@ resource "google_cloudbuild_trigger" "build_trigger_terraform" {
       args       = ["-c", "terraform plan var-file=config-${var.env}/terraform.tfvars"]
     }
   }
-
+  service_account = google_service_account.our_sa_terraform_build
 }
